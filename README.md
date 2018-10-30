212353768cccc8eda057bb6e3a9e9fe5e035108f
Unknown <louison.harizi@epitech.eu>
2018-10-30 01:52:28 +0100

    begin

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # epicture_mirror
 mirror of epicture mdr
+
+https://facebook.github.io/react-native/docs/state
--- epicture_rn/App.js
+++ epicture_rn/App.js
@@ -1,40 +1,22 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, ImageBackground, Button, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component {
+class Greeting extends React.Component {
   render() {
     return (
-      /*<View style={styles.container}>
-		<Text>PETIT PD N'OUBLI PAS</Text>
-		<Text>PETIT PD N'OUBLI PAS</Text>
-      </View>*/
-	  
-	  <ImageBackground
-            source={require("./assets/background.jpg")}
-            style={styles.container} >
-            <View
-                style={{
-                    flex: 1,
-                    justifyContent: "center",
-                    alignItems: "center"
-                }} >
-				
-				<Text>PETIT PD N'OUBLI PAS</Text>
-				<Text>PETIT PD N'OUBLI PAS</Text>
-
-            </View>
-        </ImageBackground>
-	  
-	  
+      <Text>Hello {this.props.name}!</Text>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#909090',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default class App extends React.Component {
+   render() {
+    return (
+      <View>
+        <Greeting name='Rexxar' />
+        <Greeting name='Jaina' />
+        <Greeting name='Valeera' />
+      </View>
+    );
+  }
+}
